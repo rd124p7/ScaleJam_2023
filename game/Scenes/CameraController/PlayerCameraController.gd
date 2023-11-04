bdd8119533f5ebbe9d4a5606ad7f2d3007b84282
rd124p7 <rd124p7@yahoo.com>
2023-11-04 00:25:38 -0400

    Added camera zoom out

--- game/Scenes/CameraController/PlayerCameraController.gd
+++ game/Scenes/CameraController/PlayerCameraController.gd
@@ -1,9 +1,23 @@
 extends Node2D
 
 
+# Signal to highlight the possible actions
+signal highlight_possible_actions
+
 func _ready():
 	pass
 
 
 func _process(_delta):
 	pass
+
+# Function Name: _on_monkey_action_needs_selecting
+# Description:
+#	Smoothly zoom the camera out and emit the highlight_possible_actions signal
+func _on_monkey_action_needs_selecting():
+	var camera_zoom_tween = create_tween().set_trans(Tween.TRANS_SINE)
+	camera_zoom_tween.tween_property($PlayerCamera, "zoom", Vector2(0.5, 0.5), 1)
+
+	highlight_possible_actions.emit()
+
+
